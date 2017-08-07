52ae5116b76367e9221b4bd6b59c9855b9517a18
梁城月 <2524499314@qq.com>
2017-08-07 20:15:37 +0800

    文件上传

--- Web/Controllers/StudentController.cs
+++ Web/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using Core;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,6 +25,40 @@ namespace Web.Controllers
             list.rows = l;
             list.results = 2;
             return Json(list, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Load()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase filedata)
+        {
+            if (filedata != null && filedata.ContentLength > 0)
+            {
+                //文件上传后的保存根路径 
+                string filePath = Server.MapPath("~/UploadImg/");
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+                string fileName = Path.GetFileName(filedata.FileName);//获取文件原名
+                string fileExtension = Path.GetExtension(fileName);//获取文件扩展名
+                string saveFileName = Guid.NewGuid().ToString() + fileExtension;//要保存的文件名称
+                int fileSize = filedata.ContentLength / 1024;
+                if (fileSize > 1024 || fileSize <= 2)
+                {
+                    return Json(new { ret = false, message = "文件上传失败，请选择小于1M的图片" });
+                }
+                else
+                {
+                    filedata.SaveAs(filePath + saveFileName);
+                    return Json(new { ret = true, FilePath = "/UploadImg/" + saveFileName });
+                }
+            }
+            else
+            {
+                return Json(new { ret = false, message = "请选择要上传的文件" });
+            }
         }
     }
 }
